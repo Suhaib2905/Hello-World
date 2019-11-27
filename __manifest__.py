29d04597d2c3a2619f64468e95802835f4c511b7
Suhaib2905 <58245713+Suhaib2905@users.noreply.github.com>
2019-11-27 09:56:52 +0300

    gasaoline

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Saudikom Gasoline Station',
+    'summary': """
+        Saudikom gasoline station module
+    """,
+    'description': """
+        Saudikom gasoline station module
+    """,
+    'version': '1.0',
+    'author': 'shar',
+    'depends': [
+        'base',
+        'hr',
+    ],
+    'data': [
+        'views/gasoline.xml',
+    ],
+}
